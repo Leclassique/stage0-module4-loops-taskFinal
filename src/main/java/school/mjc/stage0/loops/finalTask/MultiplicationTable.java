33315513e669f3de3adaf17ba2b7f4878c4c1d1a
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-12 00:15:00 +0300

    Completed task

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        if (numberTableToPrint <= 0) {
+            System.out.println("Input must be a positive integer.");
+            return;
+        } else {
+
+        System.out.println("Multiplication table for " + numberTableToPrint + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numberTableToPrint + " x " + i + " = " + (numberTableToPrint * i));
+        }
+    }
     }
 }
